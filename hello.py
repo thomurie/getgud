ed114bb725ca3248221fd6195aefa5e9b929926c
thomurie <70548271+thomurie@users.noreply.github.com>
2020-09-02 20:04:00 -0600

    Add descriptions
    Descriptions were added to clarify the purpose of the libraries used

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 import time
+#time is used for displaying accurate timestamps on the data 
 import numpy as np
+#numpy is used as the desired UI and UX is easily acchieveable with numpy
 import sys
+#the following is used to check for ERROR messages
 print("something")
